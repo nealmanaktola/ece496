5db7b14dc3ee23f93ed56e858537b6b70db4be85
muntaqim <muntaqim30@gmail.com>
2014-11-09 15:26:48 -0500

    two sensors for real

--- distance/distance.ino
+++ distance/distance.ino
@@ -1,7 +1,10 @@
 //Neal is the best
-int trigPin = 11;    //Trig - green Jumper
-int echoPin = 10;    //Echo - yellow Jumper
-long duration, cm, inches;
+int trigPin = 12;    //Trig - green Jumper
+int echoPin = 11;    //Echo - yellow Jumper
+
+int trigPin2 = 9;
+int echoPin2 = 8;
+long duration, duration2, cm, cm2, inches;
  
 void setup() {
   //Serial Port begin
@@ -9,19 +12,20 @@ void setup() {
   //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
 }
- 
+
 void loop()
 {
- 
- 
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5); //why?
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);`
+  digitalWrite(trigPin, LOW);
  
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
@@ -31,13 +35,35 @@ void loop()
  
   // convert the time into a distance
   cm = (duration/2) / 29.1;
-  inches = (duration/2) / 74; 
-  
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
   
   delay(50);
+  
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(5); //why?
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin2, INPUT);
+  duration2 = pulseIn(echoPin2, HIGH);
+ 
+  // convert the time into a distance
+  cm2 = (duration2/2) / 29.1;
+  
+   
+  Serial.print("Sensor 1: ");
+  Serial.print(cm);
+  Serial.print(" cm");
+  Serial.print("Sensor 2: ");
+  Serial.print(cm2);
+  Serial.print(" cm");  
+  
+  Serial.println();
+  
+  
 }
