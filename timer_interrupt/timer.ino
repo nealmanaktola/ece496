55a04bbc8491640ff1268720cd0385a16e8f0fbe
nealmanaktola <nealmanaktola@gmail.com>
2014-12-24 13:28:34 -0500

    added timer interrupt test code timer_interrupt

---
+++ timer_interrupt/timer.ino
@@ -0,0 +1,36 @@
+//Program should print consecutive numbers every second until it reaches 30.
+#include <TimerOne.h>
+
+
+#define TIMER_FREQ_US 1000000
+
+volatile int x = 0;
+int y = 0;
+
+
+
+void setup()
+{
+	Serial.begin(9600);
+	Timer1.initialize(TIMER_FREQ_US); //1 second interrupt
+	Timer1.attachInterrupt(echoCheck);
+}
+
+void loop()
+{
+	if (y != x)
+	{
+		Serial.print(x)
+		Serial.println();
+		y = x;
+	}
+	if (x == 30)
+	{
+		Timer1.stop();
+	}
+}
+
+void echoCheck()
+{
+	x++;
+}
