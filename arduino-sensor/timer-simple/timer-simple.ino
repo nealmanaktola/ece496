9cd6a2ed6f7bb470259fe77d5fa544d85e92fb7f
muntaqim <muntaqim30@gmail.com>
2015-02-16 15:13:02 -0500

    added arrays and serial print for PC

--- arduino-sensor/timer-simple/timer-simple.ino
+++ arduino-sensor/timer-simple/timer-simple.ino
@@ -6,7 +6,7 @@
 #define TIMER_FREQ_US 37
 #define PING_INTERVAL  26
 #define MAX_DISTANCE 200
-
+#define ALLOWED_DISTANCE 70
 
 short cm1;
 short cm2;
@@ -23,10 +23,10 @@ short runNum = 1;
 
 unsigned long pingTimer;
 
-short trigger1 = 0;
-short trigger2 = 0;
-short trigger3 = 0;
-short trigger4 = 0;
+short trigger1 = 1;
+short trigger2 = 1;
+short trigger3 = 1;
+short trigger4 = 1;
 
 NewPing sonar1 = NewPing(7,6,MAX_DISTANCE);
 NewPing sonar2 = NewPing(8,MAX_DISTANCE);
@@ -56,60 +56,75 @@ void loop() {
   int i;
   if (millis() >= pingTimer) {         // Is it this sensor's time to ping?
 
+    
     trigger1 = 1;
     trigger2 = 1;
     trigger3 = 1;
     trigger4 = 1;
 
     pingTimer += PING_INTERVAL;
-    //fill_arrays();
-    cm2_array[counter] = counter;
-    cm3_array[counter] = counter;
-    cm4_array[counter] = counter;
-    //counter = counter + 1;   
-   
     checkDetect();
-    print_all();
+    fill_arrays();
+//    print_all();
+ 
    
-   
+    
     sonar1.ping_interrupt(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).   
     sonar2.ping_timer(sonar1.get_max_time());
     sonar3.ping_timer(sonar1.get_max_time());
     sonar4.ping_timer(sonar1.get_max_time());
+    Timer1.resume();
     
 
-    
-
+  }
+  
+  if(!trigger1 && !trigger2 && !trigger3 && !trigger4)
+  {
+      Timer1.stop();
   }
 }
 
 void fill_arrays()
 {
-//   //cm1_array[counter] = cm1
-   cm2_array[counter] = cm2;
-   cm3_array[counter] = cm3;
-   cm4_array[counter] = cm4;
-   counter = counter + 1;
-
+   if (detect > 0)
+   {
+     if(cm2 <ALLOWED_DISTANCE)
+     {
+       cm2_array[counter] = cm2;
+     }
+     if(cm3 < ALLOWED_DISTANCE)
+     {
+       cm3_array[counter] = cm3;
+     }
+     if(cm4 < ALLOWED_DISTANCE)
+     {
+       cm4_array[counter] = cm4;
+     }
+     counter = counter + 1;
+   }
     
 }
 void checkDetect()
 {
-  if (cm2 < 70 || cm3 < 70 || cm4 < 70)
+  if (cm2 < ALLOWED_DISTANCE || cm3 < ALLOWED_DISTANCE || cm4 < ALLOWED_DISTANCE)
   {
     detect=3; // setting how many times nothing is accepted
   }
-  else if (cm2 >= 70 && cm3 >= 70 && cm4 >= 70)
+  else if (cm2 >= ALLOWED_DISTANCE && cm3 >= ALLOWED_DISTANCE && cm4 >= ALLOWED_DISTANCE)
   {
     if (detect == 1)
     {
-      Serial.println();
-      Serial.print("Run: ");
-      Serial.print(runNum);
-      Serial.println();
+//      Serial.println();
+//      Serial.print("Run: ");
+//      Serial.print(runNum);
+//      Serial.println();
       runNum ++;
       detect = 0;
-      counter = 0;   
+      counter = 0;
+      
+      //print all values
+      print_all();
+      
     }
     else if (detect==2)
     {
@@ -145,55 +160,82 @@ void echoCheck()
     trigger4 = 0;
   }	
 }
-
 void print_all()
 {
-
-//    Serial.print("1");
-//    Serial.print("=");
-//    Serial.print(cm1);
-//    Serial.print("    "); 
-// 
-  if (detect > 0)
+  int number_values = 0;
+      
+  //second sensor
+  for(number_values = 0; number_values < 20; number_values++)
   {
-    if (cm2 < 70)
-    {
-      Serial.print("2");
-      Serial.print("=");
-      Serial.print(cm2);
-      Serial.print("    ");   
-    }
-    else
-    {
-      Serial.print("        ");
-    }
-    
-    if (cm3 < 70)
-    {
-      Serial.print("3");
-      Serial.print("=");
-      Serial.print(cm3);
-      Serial.print("    ");   
-    }
-    else
-    {
-      Serial.print("        ");
-    }
-    
-    if (cm4 < 70)
-    {
-      Serial.print("4");
-      Serial.print("=");
-      Serial.print(cm4);
-      Serial.print("    ");   
-    }
-    else
-    {
-      Serial.print("        ");
-    }
-    Serial.println();
+    Serial.print(cm2_array[number_values]);
+    Serial.print(",");
   }
-  
+  Serial.print(";");
+  //third sensor
+  for(number_values = 0; number_values < 20; number_values++)
+  {
+    Serial.print(cm3_array[number_values]);
+    Serial.print(",");
+  }
+  Serial.print(";");
+  //fourth sensor
+  for(number_values = 0; number_values < 20; number_values++)
+  {
+    Serial.print(cm4_array[number_values]);
+    Serial.print(",");
+  }
+  Serial.print(";");
+  Serial.println();
+   
 }
+//void print_all()
+//{
+//
+////    Serial.print("1");
+////    Serial.print("=");
+////    Serial.print(cm1);
+////    Serial.print("    "); 
+//// 
+//  if (detect > 0)
+//  {
+//    if (cm2 < 70)
+//    {
+//      Serial.print("2");
+//      Serial.print("=");
+//      Serial.print(cm2);
+//      Serial.print("    ");   
+//    }
+//    else
+//    {
+//      Serial.print("        ");
+//    }
+//    
+//    if (cm3 < 70)
+//    {
+//      Serial.print("3");
+//      Serial.print("=");
+//      Serial.print(cm3);
+//      Serial.print("    ");   
+//    }
+//    else
+//    {
+//      Serial.print("        ");
+//    }
+//    
+//    if (cm4 < 70)
+//    {
+//      Serial.print("4");
+//      Serial.print("=");
+//      Serial.print(cm4);
+//      Serial.print("    ");   
+//    }
+//    else
+//    {
+//      Serial.print("        ");
+//    }
+//    Serial.println();
+//  }
+//  
+//}
 
 
